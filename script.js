b187fda994c608929feab43c130a11d04736ba2e
zutin <gianluccaz@hotmail.com>
2024-04-03 03:07:31 -0300

    Obtenha os dados de um planeta

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 async function getPlanets(){
   let req = await fetch('https://swapi.dev/api/planets/');
-  let planets = await req.json();
+  let {results} = await req.json();
 
-  planets.results.forEach(planet => {
+  results.forEach(planet => {
     createButton(planet);
   })
 }
 
 function createButton(planet){
+  let planetList = document.getElementById('planet-list');
+
   let button = document.createElement('button');
   button.innerHTML = planet.name;
+  button.onclick = () => fillPlanetInfo(planet);
 
-  document.body.appendChild(button);
+  planetList.appendChild(button);
+}
+
+function fillPlanetInfo(planet){
+  let name = document.getElementById('planet-name');
+  let climate = document.getElementById('climate');
+  let population = document.getElementById('population');
+  let terrain = document.getElementById('terrain');
+
+  name.innerHTML = planet.name;
+  climate.innerHTML = planet.climate;
+  population.innerHTML = planet.population;
+  terrain.innerHTML = planet.terrain;
 }
 
 getPlanets();
